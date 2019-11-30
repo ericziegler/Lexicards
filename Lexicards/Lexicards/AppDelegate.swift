391cd1f7ada5aaea5a4876295f0ebcea4917b58d
Eric <eric@zigabytes.com>
2019-11-30 15:12:55 -0500

    EZ: Initial commit.

---
+++ Lexicards/Lexicards/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  Lexicards
+//
+//  Created by Eric Ziegler on 11/30/19.
+//  Copyright © 2019 Zigabytes. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+
+}
+
---
+++ Lexicards/Lexicards/MainController.swift
@@ -0,0 +1,21 @@
+//
+//  MainController.swift
+//  Lexicards
+//
+//  Created by Eric Ziegler on 11/30/19.
+//  Copyright © 2019 Zigabytes. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
